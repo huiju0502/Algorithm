ffe387120c58eedb73a97860a5de76bdc1e14489
huiju0502 <133737044+huiju0502@users.noreply.github.com>
2023-09-17 14:34:55 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.34 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+        
+        int num1 = numbers[numbers.length-1];
+        int num2 = numbers[numbers.length-2];
+        
+        answer = num1 * num2;
+            
+        return answer;
+    }
+}
